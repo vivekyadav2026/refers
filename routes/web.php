1a18b9ff3f9be51c030a02e4dff526cc6fee2dfa
ranjeet225 <ranjeetmaurya2033@gmail.com>
2026-05-24 04:45:34 +0530

    data

--- routes/web.php
+++ routes/web.php
@@ -353,3 +353,11 @@ Route::get('/fix-storage', function () {
         return 'Error: ' . $e->getMessage();
     }
 });
+
+Route::get('/clear-cache', function() {
+    \Illuminate\Support\Facades\Artisan::call('cache:clear');
+    \Illuminate\Support\Facades\Artisan::call('config:clear');
+    \Illuminate\Support\Facades\Artisan::call('route:clear');
+    \Illuminate\Support\Facades\Artisan::call('view:clear');
+    return 'Cache cleared successfully';
+});
